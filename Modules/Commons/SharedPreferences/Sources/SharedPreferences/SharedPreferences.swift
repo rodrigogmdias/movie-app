93df389da39e029a0110cbf95c9991278c075cdc
Rodrigo Dias <rodrigogmdias@gmail.com>
2025-07-06 22:50:05 -0300

    Torna os métodos da classe SharedPreferences públicos, permitindo acesso externo às funcionalidades de armazenamento e recuperação de dados.

--- Modules/Commons/SharedPreferences/Sources/SharedPreferences/SharedPreferences.swift
+++ Modules/Commons/SharedPreferences/Sources/SharedPreferences/SharedPreferences.swift
@@ -1,25 +1,25 @@
 import Foundation
 
-class SharedPreferences {
+public class SharedPreferences {
     private let userDefaults: UserDefaults
 
-    init(userDefaults: UserDefaults = .standard) {
+    public init(userDefaults: UserDefaults = .standard) {
         self.userDefaults = userDefaults
     }
 
-    func set<T>(_ value: T, forKey key: String) {
+    public func set<T>(_ value: T, forKey key: String) {
         userDefaults.set(value, forKey: key)
     }
 
-    func get<T>(forKey key: String) -> T? {
+    public func get<T>(forKey key: String) -> T? {
         return userDefaults.object(forKey: key) as? T
     }
 
-    func removeValue(forKey key: String) {
+    public func removeValue(forKey key: String) {
         userDefaults.removeObject(forKey: key)
     }
 
-    func clear() {
+    public func clear() {
         let dictionary = userDefaults.dictionaryRepresentation()
         for key in dictionary.keys {
             userDefaults.removeObject(forKey: key)
